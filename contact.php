0aca42dfe8d879585ec674e3bac880dba1a5d5a7
leblancphil <phil@leblancphil.com>
2020-12-29 09:06:19 +0100

    Strcutures SEO - accessibilite

---
+++ contact.php
@@ -0,0 +1,131 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="Contactez notre agence pour plus d'informations">
+	<?php include 'header.html'; ?>
+    <title>Contact. Parlons de la création ou la refonte de votre site web.</title>
+
+    
+<!-- Analytics -->
+
+<!-- Analytics END -->
+    
+</head>
+<body>
+<!-- Principal conteneur -->
+<div class="page-container">
+    
+<!-- bloc-0 -->
+<?php include 'menu.html'; ?>
+<!-- bloc-0 END -->
+
+<!-- bloc-6 -->
+<div class="bloc bg-dots-bg bg-repeat bgc-atomic-tangerine d-bloc bloc-bg-texture texture-paper" id="bloc-6">
+	<div class="container bloc-lg">
+		<div class="row">
+			<div class="col-sm-12">
+				<h1 class="text-center hero-bloc-text tc-white">
+					Parlons web design !
+				</h1>
+				<p class="text-center mg-lg tc-white tight-width-whitespace">
+					Nous sommes ravis que vous souhaitiez collaborer avec notre agence.<br>
+					Parlez-nous de votre projet en complétant le formulaire ci-dessous.
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- bloc-6 END -->
+
+<!-- bloc-7 -->
+<div class="bloc bgc-white l-bloc" id="bloc-7">
+	<div class="container bloc-lg">
+		<div class="row">
+			<div class="col-sm-6">
+				<form id="form_1" novalidate success-msg="Your message has been sent." fail-msg="Sorry it seems that our mail server is not responding, Sorry for the inconvenience!">
+					<div class="form-group">
+						<label for="name">
+							Nom
+						</label>
+						<input id="name" class="form-control" required />
+					</div>
+					<div class="form-group">
+						<label for="email">
+							Adresse email
+						</label>
+						<input id="email" class="form-control" type="email" required />
+					</div>
+					<div class="form-group">
+						<label for="input_504">
+							Comment nous avez-vous connu ?
+						</label>
+						<input class="form-control" type="email" required id="input_504" />
+					</div>
+					<div class="form-group">
+						<label for="message">
+							Brief de votre projet (Ajoutez autant de détails que nécessaire)<br>
+						</label><textarea id="message" class="form-control" rows="4" cols="50" required></textarea>
+					</div> 
+					<button class="bloc-button btn btn-lg btn-block cta-hero btn-atomic-tangerine" type="submit">
+						Envoyer
+					</button>
+				</form>
+			</div>
+			<div class="col-sm-6">
+				<p>
+					contact@lachouetteagence.com<br>2 place Sathonay 69001 Lyon<br>Ouvert de 9h à 18h, du lundi au vendredi<br>
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- bloc-7 END -->
+
+<!-- ScrollToTop Button -->
+<a class="bloc-button btn btn-d scrollToTop" onclick="scrollToTarget('1')"><span class="fa fa-chevron-up"></span></a>
+<!-- ScrollToTop Button END-->
+
+
+<!-- Footer - bloc-8 -->
+<div class="bloc bgc-atomic-tangerine d-bloc" id="bloc-8">
+	<div class="container bloc-sm">
+		<div class="row">
+		<div class="col-sm-12 footer">
+				<p class="text-center">
+					La chouette agence - Agence web basée à Lyon.
+				</p>
+				<div class="row row-no-gutters social">
+					<div class="col-sm-3">
+						<div class="text-center">
+							<a class="social"  href="index.html"  aria-label="rejoignez nous sur twitter"><span class="fa fa-twitter icon-md" aria-hidden="true"></span></a>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="text-center">
+							<a class="social"  href="index.html"  aria-label="rejoignez nous sur facebook"><span class="fa fa-facebook icon-md" aria-hidden="true"></span></a>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="text-center">
+							<a class="social"  href="index.html"  aria-label="rejoignez nous sur dribbble"><span class="fa fa-dribbble icon-md" aria-hidden="true"></span></a>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="text-center">
+							<a class="social" href="index.html"  aria-label="rejoignez nous sur instagram"><span class="fa fa-instagram icon-md" aria-hidden="true"></span></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- Footer - bloc-8 END -->
+
+</div>
+<!-- Principal conteneur END -->
+    
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,259 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="'La chouette agence' est une agence de design web, création de sites web, référencement, seo, située à Lyon - France">
+	<?php include 'header.html'; ?>
+
+    <title>La chouette agence - Agence de web design - Lyon</title>
+
+
+<!-- Analytics -->
+ 
+<!-- Analytics END -->
+    
+</head>
+<body>
+<!-- Principal conteneur -->
+<div class="page-container">
+    
+<!-- bloc-0 -->
+<?php include 'menu.html'; ?>
+<!-- bloc-0 END -->
+
+<!-- bloc-1-presentation -->
+<div class="bloc bgc-dark-slate-blue bg-banniere d-bloc bg-t-edge bloc-bg-texture texture-paper b-parallax" id="bloc-1-hero">
+	<div class="container bloc-lg">
+		<div class="row tight-width-whitespace">
+			<div class="col-sm-12">
+				<img src="img/logo.png" class="center-block image-resize-mode" width="100" alt="La chouette agence, agence web paris, création logo paris" />
+				<h1 class="text-center hero-bloc-text tc-white ">
+					La chouette agence
+				</h1>
+				<div class="text-center">
+					<a href="contact.php" class="btn btn-lg btn-clean btn-rd cta-hero btn-atomic-tangerine" id="cta-hero">Contact</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- bloc-1-presentation END -->
+
+<!-- bloc-2-services -->
+<div class="bloc bgc-white l-bloc" id="bloc-2-services">
+	<div class="container bloc-md">
+		<div class="row">
+			<div class="col-xs-12 col-md-8 col-md-offset-2 text-center">
+				<h2>La chouette agence est une agence de web desing qui aide les entreprises à devenir attractives et visibles sur internet.</h2>
+			</div>
+		</div>
+		<div class="row voffset-lg med-width-whitespace">
+			<div class="col-sm-4">
+				<div class="text-center">
+					<span class="et-icon-browser sm-shadow icon-dark-slate-blue icons icon-lg"></span>
+				</div>
+				<h3 class="mg-md text-center">
+					Web design
+				</h3>
+				<p class="text-center ">
+					Que vous ayez un site web qui a besoin d'un rafraîchissement ou que vous partiez de zéro, laissez-nous vous réaliser un design dont vous serez fier.
+				</p>
+			</div>
+			<div class="col-sm-4">
+				<div class="text-center">
+					<span class="et-icon-presentation sm-shadow icon-dark-slate-blue icons icon-lg"></span>
+				</div>
+				<h3 class="mg-md text-center">
+					&nbsp;Stratégie
+				</h3>
+				<p class="text-center ">
+					Plutôt que de suggérer des designs à la mode, nous travaillerons avec vous pour comprendre vos objectifs commerciaux afin de vous proposer le meilleur design possible.
+				</p>
+			</div>
+			<div class="col-sm-4">
+				<div class="text-center">
+					<span class="et-icon-edit sm-shadow icon-dark-slate-blue icons icon-lg"></span>
+				</div>
+				<h3 class="mg-md text-center">
+					Illustrations
+				</h3>
+				<p class="text-center ">
+					Un bon design n'est rien sans de bonnes illustrations. Laissez faire notre équipe de graphistes talentueux pour donner à vos pages une touche finale d'élégance.
+				</p>
+			</div>
+		</div>
+		<div class="row voffset-lg voffset">
+			<div class="col-xs-12 col-md-8 col-md-offset-2 text-center">
+				<blockquote><h2><i>"La chouette agence est une exellente agence web! Les web designers ont réussi à capturer l'essence de notre entreprise et à l'intégrer à notre site. Cela nous a aidé à doubler nos ventes en ligne à Lyon."</i></h2><p>Maxime Guibard, PDG de A vos fourchettes</p></blockquote>
+
+			</div>
+		</div>
+	</div>
+</div>
+<!-- bloc-2-services END -->
+
+<!-- bloc-3-what-i-do -->
+<div class="bloc tc-white bgc-atomic-tangerine bg-presentation d-bloc bloc-bg-texture texture-paper b-parallax" id="bloc-3-what-i-do">
+	<div class="container bloc-lg">
+		<div class="row tight-width-whitespace black-background">
+			<div class="col-sm-12">
+				<h2 class="mg-md text-center tc-white">
+					Nous souhaitons aider les entreprises de la région Lyonnaise à se développer et à connaître le succès. Nous travaillons pour aider les entreprises locales à attirer de nouveaux clients .
+				</h2>
+				<p class="text-center white">
+					Nous aimons collaborer avec des entrepreneurs créatifs et des entreprises locale, qu’elles aient besoin de créer un site de A à Z ou de donner un coup de jeune à leur site. Que vous ayez besoin d’une identité visuelle complète ou d’une refonte de votre site, n’hésitez pas à nous contacter. Nous travaillerons ensemble pour créer des sites beau et bien structurés, qui seront facilement trouvable sur les moteurs de recherche. </p><p><a class="ltc-white bold-link" href="page2.html">Contactez notre équipe</a></p>
+				
+			</div>
+		</div>
+	</div>
+</div>
+<!-- bloc-3-what-i-do END -->
+
+<!-- bloc-4-portfolio -->
+<div class="bloc bgc-white bg-lines-h2-bg bg-repeat l-bloc" id="bloc-4-portfolio">
+	<div class="container bloc-lg">
+		<div class="row">
+			<div class="col-sm-12 text-center">
+				<h2> Nous travaillons avec beaucoup de clients,<br>voici nos réalisations</h2>
+			</div>
+		</div>
+		<div class="row tight-width-whitespace portfolio-row">
+			<div class="col-sm-6">
+				<a href="#" data-lightbox="img/magazines-papier.jpg" data-gallery-id="gallery-1" data-caption="Refonte d'un site web pour un journal local" data-frame="snapshot-lb"><img src="img/thumbs/magazines-papier.jpg" alt="refonte du site web d'un journal" class="img-responsive portfolio-thumb" /></a>
+				<h3 class="mg-md text-center">
+					Refonte d'un site web pour un journal local
+				</h3>
+				<p class="text-center">
+					Nous avons créé une nouvelle identité visuelle pour un journal lyonnais.
+				</p>
+			</div>
+			<div class="col-sm-6">
+				<a href="#" data-lightbox="img/photographe-prise-de-vue.jpg" data-gallery-id="gallery-1" data-caption="Création d'un site web pour photographes" data-frame="snapshot-lb"><img src="img/thumbs/photographe-prise-de-vue.jpg" class="img-responsive portfolio-thumb" alt="Création d'un site web pour photographes" /></a>
+				<h3 class="mg-md text-center">
+					Création d'un site web pour photographes
+				</h3>
+				<p class="text-center">
+					Nous avons conçu la plateforme idéale pour laisser place aux clichés des photographes.
+				</p>
+			</div>
+		</div>
+		<div class="row tight-width-whitespace portfolio-row">
+			<div class="col-sm-6">
+				<a href="#" data-lightbox="img/objets-voyageurs.jpg" data-gallery-id="gallery-1" data-caption="Création d'un site internet pour un voyageur" data-frame="snapshot-lb"><img src="img/thumbs/objets-voyageurs.jpg" class="img-responsive portfolio-thumb" alt="Création d'un site web pour voyageur"/></a>
+				<h3 class="mg-md text-center">
+					Création d'un site internet pour un voyageur
+				</h3>
+				<p class="text-center">
+					Nous avons élaboré un site internet et des illustrations pour sublimer les vidéos et les articles de ce voyageur.
+				</p>
+			</div>
+			<div class="col-sm-6">
+				<a href="#" data-lightbox="img/agence-mariages.jpg" data-gallery-id="gallery-1" data-caption="Conception d'un site pour une agence de mariage" data-frame="snapshot-lb"><img src="img/thumbs/agence-mariages.jpg" class="img-responsive portfolio-thumb" alt="Création d'un site web pour agence de mariage" /></a>
+				<h3 class="mg-md text-center">
+					Conception d'un site pour une agence de mariage
+				</h3>
+				<p class="text-center">
+					Nous avons travaillé avec cette agence pour créer un site internet permettant la présentation détaillée de leurs prestations.
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- bloc-4-portfolio END -->
+
+<!-- bloc-5-cta -->
+<div class="bloc bgc-dark-slate-blue bg-banniere d-bloc bloc-bg-texture texture-paper" id="bloc-5-cta">
+	<div class="container bloc-lg">
+		<div class="row">
+			<h2 class="mg-md text-center tc-white">
+				Prêt pour redonner vie à votre projet ?<br>Contactez notre équipe !
+			</h2>
+			<div class="col-sm-12 text-center">
+				<a href="contact.php" class="btn btn-atomic-tangerine btn-clean btn-rd btn-lg cta-hero">Contact</a>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- bloc-5-cta END -->
+
+<!-- Footer - bloc-8 -->
+<div class="bloc bgc-atomic-tangerine d-bloc" id="bloc-8">
+	<div class="container bloc-sm">
+		<div class="row">
+			<div class="col-sm-12 footer">
+				<p class="text-center">
+					La chouette agence - Agence web basée à Lyon.
+				</p>
+				<div class="row row-no-gutters social">
+					<div class="col-sm-3">
+						<div class="text-center">
+							<a class="social"  href="index.html"  aria-label="rejoignez nous sur twitter"><span class="fa fa-twitter icon-md" aria-hidden="true"></span></a>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="text-center">
+							<a class="social"  href="index.html"  aria-label="rejoignez nous sur facebook"><span class="fa fa-facebook icon-md" aria-hidden="true"></span></a>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="text-center">
+							<a class="social"  href="index.html"  aria-label="rejoignez nous sur dribbble"><span class="fa fa-dribbble icon-md" aria-hidden="true"></span></a>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="text-center">
+							<a class="social" href="index.html"  aria-label="rejoignez nous sur instagram"><span class="fa fa-instagram icon-md" aria-hidden="true"></span></a>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-4">
+						Partenaires
+							<ul>
+								<li><a href="voiture.com">voiture.com</a></li>
+								<li><a href="pizza.com">pizza.com</a></li>
+								<li><a href="matoiture.com">matoiture.com</a></li>
+								<li><a href="immobilierseo.com">immobilierseo.com</a></li>
+							</ul>		
+					</div>
+					<div class="col-sm-4">
+						Annuaires liste 1
+						<ul>
+							<li><a href="annuaireagence.com">annuaireagence.com</a></li>
+							<li><a href="annuaireseo.com">annuaireseo.com</a></li>
+							<li><a href="annuairemarketing.com">annuairemarketing.com</a></li>
+							<li><a href="123seoture.com">123seo.com</a></li>
+							<li><a href="123site.com">123site.com</a></li>
+							<li><a href="annuairewordpress.com">annuairewordpress.com</a></li>
+							<li><a href="listeagence.com">listeagence.com</a></li>
+							<li><a href="meilleuresagencesparis.com">meilleuresagencesparis.com</a></li>
+							<li><a href="listeseo.com">listeseo.com</a></li>
+						</ul>
+					</div>
+					<div class="col-sm-4">
+						Annuaires liste 2
+						<ul>
+							<li><a href="betterseo.com">betterseo.com</a></li>
+							<li><a href="ameliorerseo.com">ameliorerseo.com</a></li>
+							<li><a href="seojuicelien.com">seojuicelien.com</a></li>
+							<li><a href="meilleursliens.com">meilleursliens.com</a></li>
+							<li><a href="liensagogo.com">liensagogo.com</a></li>
+							<li><a href="backlink.com">backlink.com</a></li>
+							<li><a href="linkjuiceannuaire.com">linkjuiceannuaire.com</a></li>
+							<li><a href="remontersurgoogle.com">remontersurgoogle.com</a></li>
+							<li><a href="vraiseofiable.com">vraiseofiable.com</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- Footer - bloc-8 END -->
+
+</div>
+<!-- Principal conteneur END -->
+    
+
+</body>
+</html>
